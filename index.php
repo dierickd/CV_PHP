96fb2f740d4adeb16c07e726e2069248c24e8b22
DIERICK Dewi <dierickd@outlook.fr>
2020-03-18 18:35:40 +0100

    Create projet, first push

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    include 'template/header.php';
+    include 'template/navbar.php';
+?>
+<main class="main">
+    <?php
+    include "template/about.php";
+    include "template/skills.php";
+    include "template/portfolio.php";
+    include "template/training.php";
+    ?>
+</main>
+<?php
+    include 'template/form.php';
+?>
+</body>
+</html>

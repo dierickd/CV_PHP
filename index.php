4d1d6d64a2ac19983c02645ce0694679ed463dd1
DIERICK Dewi <dierickd@outlook.fr>
2020-03-19 18:23:53 +0100

    added header style (background, effects...)

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/bfe9f51c30.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
--- template/header.php
+++ template/header.php
@@ -1,3 +1,14 @@
 <header>
-
+    <div class="name_presentation">
+        <h1>
+            DIERICK Déwi
+            <span>Etudiant Développeur Web</span>
+        </h1>
+    </div>
+    <div class="mouse">
+        <i class="fas fa-chevron-down"></i>
+        <i class="fas fa-chevron-down"></i>
+        <i class="fas fa-chevron-down"></i>
+    </div>
+    <p class="scroll">Scroll down</p>
 </header>

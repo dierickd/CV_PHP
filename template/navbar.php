d2259f1ffdcf4eb18fdd52b2dd002113b2c9688b
DIERICK Dewi <dierickd@outlook.fr>
2020-03-19 19:28:53 +0100

    changing the 'scroll down' style

---
+++ template/navbar.php
@@ -0,0 +1,13 @@
+<nav class="nav-bar" id="sticky">
+    <ul class="links" id="links">
+        <li><a href="#top" class="nav-link" title="retour a l'accueil">Accueil</a></li>
+        <li><a href="#experience" class="nav-link" title="section expérience">Experiences</a></li>
+        <li><a href="#skills" class="nav-link" title="section compétences">Compétences</a></li>
+        <li><a href="#portfolio" class="nav-link" title="section portfolio">Réalisations</a></li>
+        <li><a href="#training" class="nav-link" title="section formations">Formations</a></li>
+        <li><a href="#contact" class="nav-link" title="section contact">Me contacter</a></li>
+    </ul>
+    <a href="#links" class="btn-mobile">
+        <i class="fas fa-bars"></i>
+    </a>
+</nav>
